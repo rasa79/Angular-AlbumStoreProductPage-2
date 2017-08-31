78fec81987d2c32ebe09a6168a1f56c7f0e6a774
carolinapowers <carolinapoloni@gmail.com>
2017-08-31 16:35:40 -0400

    Declare since as a variable and give it a type in typings.d.ts, so TypeScript stops complaning about it not being named

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,6 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// declare the type for since, which is a global name added by jasmine2-custom-message plugin
+declare var since: any;
