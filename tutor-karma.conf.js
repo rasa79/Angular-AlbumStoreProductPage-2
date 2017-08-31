9ae75d7acd62a602edb28a87efd3b7c6d351a664
carolinapowers <carolinapoloni@gmail.com>
2017-08-31 15:02:43 -0400

    Add outputFile option to jsonResultReporter so we can read out the report at the generated file

--- tutor-karma.conf.js
+++ tutor-karma.conf.js
@@ -2,9 +2,9 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 const argv = require('minimist')(process.argv.slice(2)),
-  part = (argv.part !== true) && argv.part;
+  part = argv.part !== true && argv.part;
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -16,29 +16,27 @@ module.exports = function (config) {
       require('karma-structured-json-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       args: [part],
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    files: [
-      { pattern: './src/test.ts', watched: false }
-    ],
+    files: [{ pattern: './src/test.ts', watched: false }],
     preprocessors: {
       './src/test.ts': ['@angular/cli']
     },
+
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['json-result'],
+    reporters:
+      config.angularCli && config.angularCli.codeCoverage ? ['progress', 'coverage-istanbul'] : ['json-result'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -46,6 +44,7 @@ module.exports = function (config) {
     browsers: ['PhantomJS'],
     singleRun: false,
     jsonResultReporter: {
+      outputFile: 'karma-result.json',
       isSynchronous: true
     }
   });
