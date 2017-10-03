150ecdba13f98c5fbad0651d7e203fa9ad00e61f
Jon Friskics <jonfriskics@gmail.com>
2017-10-03 14:00:43 -0400

    switch part6 mocha specs from expect to assert

--- src/spec/mocha-specs/part6/app-module-approutes-array-contains-correct-object1.mocha.spec.js
+++ src/spec/mocha-specs/part6/app-module-approutes-array-contains-correct-object1.mocha.spec.js
@@ -1,15 +1,22 @@
 let fs = require('fs');
 let expect = require('chai').expect
+let assert = require('chai').assert
 
 describe('AppModule', function() {
   it(`should have a const array named appRoutes where index 0 contains an object with the correct keys and values @app-module-approutes-array-contains-correct-object1`, function () {
-    let file = fs.readFileSync(__dirname + '/../../app/app.module.ts').toString();
+    let file;
+    try {
+      file = fs.readFileSync(__dirname + '/../../../app/app.module.ts').toString();
+    } catch (e) {
+      assert(false, "There is no `app.module.ts` file for some strange reason.")
+    }
     let re = /const\s+appRoutes\s*\:\s*Routes\s*\=\s*\[([\w\s\:\'\"\,\{\}\/\;]*)\]\;?\s*\@NgModule/
     let match = file.match(re);
-
+    
     let match_trimmed = match[1].trim();    
     let re2 = /\{\s*(?:\w+)\s*\:\s*(?:\'|\")(?:[\w\/\:]+)(?:\'|\")\s*\,\s*(?:\w+)\s*\:\s*(?:\w+)\s*\}\,?/g
     let match2 = match_trimmed.match(re2);
+    assert(Array.isArray(match2), "You haven't added any objects with keys and values to the `appRoutes` array yet.");
 
     let re3 = /\{\s*(\w+)\s*\:\s*(?:\'|\")([\w\/\:]+)(?:\'|\")\s*\,\s*(\w+)\s*\:\s*(\w+)\s*\}/
     let match3 = match2[0].match(re3)
@@ -19,9 +26,9 @@ describe('AppModule', function() {
     let key2 = match3[3];
     let value2 = match3[4];
 
-    expect(key1).equals('path');
-    expect(value1).equals('products');
-    expect(key2).equals('component');
-    expect(value2).equals('ProductListComponent');
+    assert(key1 == 'path', "The first object in the `appRoutes` constant doesn't have a `path` key.");
+    assert(value1 == 'products', "The first object in the `appRoutes` constant's path key doesn't have a value of `products`.");
+    assert(key2 == 'component', "The first object in the `appRoutes` constant doesn't have a `component` key.");
+    assert(value2 == 'ProductListComponent', "The first object in the `appRoutes` constant's component key doesn't have a value of `ProductListComponent`.");
   });
 });
--- src/spec/mocha-specs/part6/app-module-ngmodule-imports-approutes.mocha.spec.js
+++ src/spec/mocha-specs/part6/app-module-ngmodule-imports-approutes.mocha.spec.js
@@ -1,12 +1,18 @@
 let fs = require('fs');
 let expect = require('chai').expect
+let assert = require('chai').assert
 
 describe('AppModule', function() {
   it(`should add the appRoutes to the NgModule imports section @app-module-ngmodule-imports-approutes`, function () {
-    let file = fs.readFileSync(__dirname + '/../../app/app.module.ts').toString();
+    let file;
+    try {
+      file = fs.readFileSync(__dirname + '/../../../app/app.module.ts').toString();
+    } catch (e) {
+      assert(false, "There is no `app.module.ts` file for some strange reason.")
+    }
     let re = /imports\s*\:\s*\[\s*([\w\s\(\)\.\,]+)\]/g
     let match = re.exec(file);
-    expect(match).to.be.an('array');
+    assert(Array.isArray(match), "There isn't an `imports` array in the `@NgModule` for some reason.")
 
     let arr = match[1].split(',');
     for (let i = 0; i < arr.length; i++) {
@@ -15,6 +21,6 @@ describe('AppModule', function() {
 
     console.log(arr);
 
-    expect(arr).to.be.an('array').that.includes('RouterModule.forRoot(appRoutes)');
+    assert(Array.isArray(arr) && arr.includes('RouterModule.forRoot(appRoutes)'), "The `appRoutes` array hasn't been added in the `@NgModule`'s `imports` array.");
   });
 });
