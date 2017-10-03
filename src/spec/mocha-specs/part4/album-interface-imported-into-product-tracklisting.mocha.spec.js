d8e126108cd0336329baa9e223ce117f7fe4ce97
Jon Friskics <jonfriskics@gmail.com>
2017-10-03 09:58:06 -0400

    switch part4 mocha specs from expect to assert

--- src/spec/mocha-specs/part4/album-interface-imported-into-product-tracklisting.mocha.spec.js
+++ src/spec/mocha-specs/part4/album-interface-imported-into-product-tracklisting.mocha.spec.js
@@ -1,10 +1,16 @@
 let fs = require('fs');
 let expect = require('chai').expect
+let assert = require('chai').assert
 
 describe('ProductTracklisting', function() {
-  it(`should import the Album Inteface @album-interface-imported-into-product-tracklisting`, function () {
-    let file = fs.readFileSync(__dirname + '/../../app/product-tracklisting/product-tracklisting.component.ts').toString();
+  it(`should import the Album Interface @album-interface-imported-into-product-tracklisting`, function () {
+    let file;
+    try {
+      file = fs.readFileSync(__dirname + '/../../../app/product-tracklisting/product-tracklisting.component.ts').toString();
+    } catch (e) {
+      assert(false, "ProductTracklistingComponent doesn't exist yet.")
+    }
     let re = /import\s*{\s*Album\s*}\s*from\s*[\'|\"]\.\.\/album[\'|\"]\;/
-    expect(file.match(re)).to.be.an('array').and.to.not.be.null;
+    assert(Array.isArray(file.match(re)) && file.match(re) != null, "The Album Interface hasn't been imported into the ProductTracklistingComponent yet.");
   });
 });
--- src/spec/mocha-specs/part4/product-service-imported-into-product-tracklisting.mocha.spec.js
+++ src/spec/mocha-specs/part4/product-service-imported-into-product-tracklisting.mocha.spec.js
@@ -1,10 +1,16 @@
 let fs = require('fs');
 let expect = require('chai').expect
+let assert = require('chai').assert
 
 describe('ProductTracklisting', function() {
   it(`should import the ProductService @product-service-imported-into-product-tracklisting`, function () {
-    let file = fs.readFileSync(__dirname + '/../../app/product-tracklisting/product-tracklisting.component.ts').toString();
+    let file
+    try {
+      file = fs.readFileSync(__dirname + '/../../../app/product-tracklisting/product-tracklisting.component.ts').toString();
+    } catch (e) {
+      assert(false, "The ProductTracklistingComponent hasn't been created yet.")
+    }
     let re = /import\s*{\s*ProductService\s*}\s*from\s*[\'|\"]\.\.\/product\.service[\'|\"]\;/
-    expect(file.match(re)).to.be.an('array').and.to.not.be.null;
+    assert(Array.isArray(file.match(re)) && file.match(re) != null, "The ProductService hasn't been imported into the ProductTracklisting yet.");
   });
 });
