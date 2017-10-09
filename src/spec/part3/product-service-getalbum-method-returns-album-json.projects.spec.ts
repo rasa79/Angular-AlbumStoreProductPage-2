75cd9d5dbfc491102118a05cdce0cf69b3f29784
Jon Friskics <jonfriskics@gmail.com>
2017-10-09 16:22:24 -0400

    fix subproject 3 test that checks if getAlbum() returns JSON

--- src/spec/part3/product-service-getalbum-method-returns-album-json.projects.spec.ts
+++ src/spec/part3/product-service-getalbum-method-returns-album-json.projects.spec.ts
@@ -22,7 +22,9 @@ try {
 }
 
 class AProductService {
-  
+  getAlbum() {
+
+  }
 }
 
 describe('ProductService', () => {
@@ -68,8 +70,11 @@ describe('ProductService', () => {
     });
     if(product_service.getAlbum == undefined) {
       since('The ProductService doesn\'t have a method named `getAlbum` yet').expect(0).toBe(1);
+    } else if(product_service.getAlbum != undefined && product_service.getAlbum.subscribe == undefined) {
+      let ps = product_service.getAlbum(null);
+      since('It doesn\'t look like you\'re returning the result of calling `this._http.get()` and passing `this._albumUrl` as a parameter').expect(product_service._http._backend.connectionsArray.length).toBeGreaterThan(0);
+      since('It doesn\'t look like you\'re returning the result of calling `this._http.get()` and passing `this._albumUrl` as a parameter').expect(product_service._http._backend.connectionsArray[0].request.url).toBe('../assets/album.json');        
     } else {
-      product_service.getAlbum(null).subscribe();
     }
   }));
 });
