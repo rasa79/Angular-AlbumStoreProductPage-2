9568b42c8360c29d11393c788d7e3bdaace510d8
Jon Friskics <jonfriskics@gmail.com>
2017-11-13 13:23:44 -0500

    solution: Displaying a List of Products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
