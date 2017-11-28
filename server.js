220536e3dc9e113b8e9cb832a53eab6728ab7e3e
Jon Friskics <jonfriskics@gmail.com>
2017-11-28 12:48:39 -0500

    fix wildcard route

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-// app.use(express.static(__dirname + '/dist'));
-app.use(__dirname + '/dist*', express.static(path.join(process.cwd(), environment)));
+app.use(express.static(__dirname + '/dist/*'));
+// app.use(__dirname + '/dist*', express.static(path.join(process.cwd(), environment)));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
