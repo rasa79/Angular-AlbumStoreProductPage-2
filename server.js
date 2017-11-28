0ff0440b08b3604361e56531f9f3c63b4474a081
Jon Friskics <jonfriskics@gmail.com>
2017-11-28 12:47:23 -0500

    more path testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 // app.use(express.static(__dirname + '/dist'));
-app.use('/dist*', express.static(path.join(process.cwd(), environment)));
+app.use(__dirname + '/dist*', express.static(path.join(process.cwd(), environment)));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
