faedd62c6e46c2cddf6306ab3feedcae3a712043
Jon Friskics <jonfriskics@gmail.com>
2017-11-30 15:19:12 -0500

    reset server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+// server.js
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/*'));
-// app.use(__dirname + '/dist*', express.static(path.join(process.cwd(), environment)));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
