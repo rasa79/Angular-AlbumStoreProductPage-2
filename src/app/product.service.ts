cada9710906d1e4f1dd5a19abd8f8b12091c6625
Jon Friskics <jonfriskics@gmail.com>
2019-04-23 23:44:20 -0400

    module2-solution

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
+  }
+}
