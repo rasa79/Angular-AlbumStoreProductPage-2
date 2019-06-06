96d6534212c1077d0cf781388adf84c6529a71b5
Meg Kline-Tuls <meghan-klinetuls@pluralsight.com>
2019-06-06 15:13:24 -0500

    fix(testtag): adds missing test tag to test block mod6 task1

--- src/spec/mocha-specs/part6/product-list-component-created.spec.js
+++ src/spec/mocha-specs/part6/product-list-component-created.spec.js
@@ -1,7 +1,7 @@
 const helpers = require("../helpers");
 
 describe("Project", () => {
-  it("The ProductListComponent doesn't exist - have you run the `ng` command to generate it yet?", () => {
+  it("should create the product list component @product-list-component-created", () => {
     helpers.readFile(
       "src/app/product-list/product-list.component.ts",
       "The ProductListComponent doesn't exist - have you run the `ng` command to generate it yet?"
